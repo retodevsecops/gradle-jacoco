5af136fd1acbb81f031eaac52140c6464c31b778
it-cmartinezv <excvillada@consubanco.com>
2024-05-26 14:46:38 -0500

    add fix

--- domain/usecase/src/main/java/com/consubanco/usecase/loan/BuildDataForApplicationUseCase.java
+++ domain/usecase/src/main/java/com/consubanco/usecase/loan/BuildDataForApplicationUseCase.java
@@ -2,9 +2,9 @@ package com.consubanco.usecase.loan;
 
 import com.consubanco.model.commons.exception.factory.ExceptionFactory;
 import com.consubanco.model.entities.document.gateway.PayloadDocumentGateway;
-import com.consubanco.model.entities.file.File;
 import com.consubanco.model.entities.file.constant.FileConstants;
 import com.consubanco.model.entities.file.gateway.FileRepository;
+import com.consubanco.model.entities.file.vo.FileWithStorageRouteVO;
 import com.consubanco.model.entities.process.Process;
 import com.consubanco.usecase.document.GenerateNom151UseCase;
 import lombok.RequiredArgsConstructor;
@@ -25,7 +25,7 @@ public class BuildDataForApplicationUseCase {
 
     public Mono<Map<String, Object>> execute(Process process) {
         Mono<Map<String, Object>> allDataMap = payloadDocGateway.getAllData(process.getId());
-        Mono<List<File>> allFiles = getAllFiles(process);
+        Mono<List<FileWithStorageRouteVO>> allFiles = getAllFiles(process);
         return Mono.zip(allDataMap, allFiles)
                 .map(tuple -> {
                     tuple.getT1().put("files_data", tuple.getT2());
@@ -34,19 +34,18 @@ public class BuildDataForApplicationUseCase {
 
     }
 
-    private Mono<List<File>> getAllFiles(Process process) {
+    private Mono<List<FileWithStorageRouteVO>> getAllFiles(Process process) {
         return generateNom151UseCase.execute(process)
                 .flatMap(files -> getOfferFiles(process));
     }
 
-    private Mono<List<File>> getOfferFiles(Process process) {
+    private Mono<List<FileWithStorageRouteVO>> getOfferFiles(Process process) {
         return Mono.just(process.getOfferId())
                 .map(FileConstants::offerDirectory)
-                .flatMapMany(fileRepository::listByFolderWithoutUrls)
+                .flatMapMany(fileRepository::listByFolder)
                 .collectList()
                 .filter(files -> !files.isEmpty())
                 .switchIfEmpty(ExceptionFactory.monoBusiness(FILES_NOT_FOUND, requiredFiles(process.getOfferId())));
     }
-
 
 }
