85b2259fe5d49e1504a4a663c6dfc145ece5724c
it-cmartinezv <excvillada@consubanco.com>
2024-05-29 16:36:34 -0500

    add fix

--- infrastructure/entry-points/api/src/main/java/com/consubanco/api/services/file/constants/FilePaths.java
+++ infrastructure/entry-points/api/src/main/java/com/consubanco/api/services/file/constants/FilePaths.java
@@ -9,7 +9,7 @@ public class FilePaths {
     public static final String GET_AND_UPLOAD_DOCUMENT_PATH = "/document/generate-and-upload/{processId}";
     public static final String CNCA_PATH = "/offer/cnca/{processId}";
     public static final String DOCUMENTS_AGREEMENT_PATH = "/offer/process/{processId}";
-    public static final String FILES_OFFER_PATH = "/offer/{offerId}";
+    public static final String FILES_OFFER_PATH = "/offer/{processId}";
     public static final String FILES_CUSTOMER_VIEW_PATH = "/offer/customer-view/{processId}";
     public static final String PAYLOAD_TEMPLATE_PATH = "/management/upload-payload-template";
     public static final String AGREEMENTS_CONFIG_PATH = "/management/upload-agreements-config";
